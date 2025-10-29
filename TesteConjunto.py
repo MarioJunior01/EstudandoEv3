a0c31e239c2eafc873d8ccbf887902fc4d3c69c2
.l <136728961+MarioJunior01@users.noreply.github.com>
2025-10-29 10:08:27 -0400

    correto

--- TesteConjunto.py
+++ TesteConjunto.py
@@ -1,20 +1,21 @@
+#!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, ColorSensor, UltrasonicSensor
+from pybricks.ev3devices import Motor, ColorSensor, InfraredSensor
 from pybricks.parameters import Port, Color
 from pybricks.tools import wait
 
 ev3 = EV3Brick()
-motorA = Motor(Port.A)
+motorA = Motor(Port.C)
 motorB = Motor(Port.B)
-sensor_Ir = UltrasonicSensor(Port.S3)
+sensor_Ir = InfraredSensor(Port.S3)
 sensor_corEs = ColorSensor(Port.S1)
 sensor_corDr = ColorSensor(Port.S2)
 
 
 velocidade = 300
 velocidade_curva = 200
-distancia_obstaculo = 20
-
+distancia_obstaculo = 15
+ultima_correcao="centro"
 
 
 desviando = False
@@ -38,12 +39,12 @@ def virarEsquerda(vel=velocidade_curva):
     motorA.run(-vel)
 
 def curvaSuaveDireita():
-    # Curva mais suave - motor direito mais lento
+    
     motorA.run(velocidade)
     motorB.run(velocidade * 0.3)
 
 def curvaSuaveEsquerda():
-    # Curva mais suave - motor esquerdo mais lento
+    
     motorA.run(velocidade * 0.3)
     motorB.run(velocidade)
 
@@ -51,87 +52,99 @@ def desviarObj():
     global desviando
     desviando = True
     
-    ev3.speaker.beep()  # Aviso sonoro
+    ev3.speaker.beep()  
     
-    # Curva 1 - direita
     virarDireita()
     wait(600)
     
-    # Andar reto contornando obstáculo
-    andar()
-    wait(1500)
     
-    # Curva 2 - esquerda
+    andar()
+    wait(2000)
+    
+    
+    virarEsquerda()
+    wait(700)
+    
+    
+    andar()
+    wait(2000)
+    
     virarEsquerda()
     wait(600)
+   
+    while corEs!= Color.BLACK and  corDr != Color.BLACK:
+       corDr = sensor_corDr.color()
+       corEs = sensor_corEs.color()
+       andar()
+       wait(1000)
+      
+      
+     
+       if corDr == Color.BLACK or corEs == Color.BLACK:
+         ev3.speaker.beep(1000)  
+         main()
+         
+        
+ 
     
-    # Andar reto
+    
+    
     andar()
-    wait(1900)
-    # Curva 3 - esquerda
-    virarEsquerda()
-    wait(600)
+    wait(2000)
     
-    # Andar reto
-    andar()
-    wait(1200)
-    
-    # Curva 4 - direita para realinhar
     virarDireita()
-    wait(600)
+    wait(1000) 
     
-    parar()
-    desviando = False
+    andar()
+    
+    
+        
 
 def seguirLinha():
+     global ultima_correcao 
+     
+     corDr = sensor_corDr.color()
+     corEs = sensor_corEs.color()
+    
+    
 
-    
-    corDr = sensor_corDr.color()
-    corEs = sensor_corEs.color()
-    
-    
-
-    if corEs == Color.BLACK and corDr == Color.BLACK:
-        # Ambos na linha - seguir reto
+     if corEs == Color.BLACK and corDr == Color.BLACK:
+       
         andar()
         ultima_correcao = "centro"
         
-    elif corEs == Color.WHITE and corDr == Color.BLACK:
-        # Esquerda saiu da linha - curva suave à direita
+     elif corEs == Color.WHITE and corDr == Color.BLACK:
+       
         curvaSuaveDireita()
         ultima_correcao = "direita"
         
-    elif corEs == Color.BLACK and corDr == Color.WHITE:
-        # Direita saiu da linha - curva suave à esquerda
+     elif corEs == Color.BLACK and corDr == Color.WHITE:
+       
         curvaSuaveEsquerda()
         ultima_correcao = "esquerda"
         
-    elif corEs == Color.WHITE and corDr == Color.WHITE:
-        # Ambos fora da linha - estratégia de recuperação
+     elif corEs == Color.WHITE and corDr == Color.WHITE:
+       
         if ultima_correcao == "direita":
             virarEsquerda()
             wait(150)
         elif ultima_correcao == "esquerda":
             virarDireita()
             wait(150)
-        else:
-            # Busca padrão - girar até encontrar linha
-            virarDireita()
-            wait(300)
+    
         parar()
         
-    else:
-        # Para cores não mapeadas, seguir reto
-        andar()
-
-while True:
+def main ():
+ while True:
     if desviando == False:
          distanciaObj = sensor_Ir.distance()
         
-        if distanciaObj <= distancia_obstaculo:
-            parar()
-            desviarObj()
-        else:
-            seguirLinha()
+         if distanciaObj <= distancia_obstaculo:
+             parar()
+             desviarObj()
+         else:
+             seguirLinha()
     
     wait(10)
+
+main()
